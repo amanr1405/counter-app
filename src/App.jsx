b7131b48c2d914ccafbfa7fdb31396883347b6a4
Aman <81345472+amanr1405@users.noreply.github.com>
2025-05-29 16:56:48 +0530

    error displayed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import Tech from './techstack';
 import './App.css';
 
 function Demo() {
-  const [a, b] = useState(0);
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState('');
+
   function addOn() {
-    b(prev => prev + 1);
+    if (count >= 10) {
+      setError("Can't go above 10");
+    } else {
+      setCount(prev => prev + 1);
+      setError('');
+    }
   }
 
   function subOn() {
-    b(prev => prev - 1);
+    if (count <= 0) {
+      setError("Can't go below 0");
+    } else {
+      setCount(prev => prev - 1);
+      setError('');
+    }
   }
 
   return (
@@ -23,13 +35,14 @@ function Demo() {
           </div>
           <div className="container_sub1_partb">
             <div className="text_button_a">
-              <h1 className='text'>{a}</h1>
+              <h1 className='text'>{count}</h1>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
             </div>
             <div className="text_button_b">
               <button className='incre' onClick={addOn}>Add <span>One</span></button>
               <button className='decre' onClick={subOn}><span>Sub</span> One</button>
             </div>
-            <Tech/>
+            <Tech />
           </div>
         </div>
       </div>
