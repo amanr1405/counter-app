8f7b72d24232f1527a9b72b610cf7cf7634df764
Aman <81345472+amanr1405@users.noreply.github.com>
2025-05-29 14:08:26 +0530

    Edited

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Tech from './techstack';
+import './App.css';
+
+function Demo() {
+  const [a, b] = useState(0);
+  function addOn() {
+    b(prev => prev + 1);
+  }
+
+  function subOn() {
+    b(prev => prev - 1);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="container_sub1">
+          <div className="container_sub1_parta">
+            <h1 className='rotate-text'>Counter <span>App</span></h1>
+          </div>
+          <div className="container_sub1_partb">
+            <div className="text_button_a">
+              <h1 className='text'>{a}</h1>
+            </div>
+            <div className="text_button_b">
+              <button className='incre' onClick={addOn}>Add One</button>
+              <button className='decre' onClick={subOn}>Sub One</button>
+            </div>
+            <Tech/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Demo;
---
+++ src/techstack.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+
+function Tech(){
+    return(  
+    <>
+        <div className="calculator_container1_2">
+            <h3>Tech <span>Stack</span></h3>
+            <br />
+            <div className="calculator_container1_2_img">
+                <img src="src\assets\picture\html.png" alt="html"/>
+                <img src="src\assets\picture\css.png" alt="css"/>
+                <img src="src\assets\picture\js.png" alt="js"/>
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default Tech
